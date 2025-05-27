e02d841e448e51c49bfa5489dd4b144b9551ced3
Douglas Ramos Pimenta <douglasramospimenta@gmail.com>
2025-05-27 13:43:37 -0300

    Create Equipamento.js

---
+++ backend/models/Equipamento.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const EquipamentoSchema = new mongoose.Schema(
+  {
+    nome: { type: String, required: true },
+    descricao: String,
+    ptrimonio: String,
+    numeroSerie: String,
+    qrCodeId: { type: String, unique: true },
+    status: {
+      type: String,
+      enum: ["ativo", "inativo", "manutencao"],
+      default: "ativo",
+    },
+    ambiente: { type: mongoose.Schema.Types.ObjectId, ref: "Ambiente" },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Equipamento", EquipamentoSchema);
