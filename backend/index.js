9ea1331806a8e6c1dc653e97840b988949a517aa
Douglas Ramos Pimenta <douglasramospimenta@gmail.com>
2025-05-29 19:30:35 -0300

    Adiciona app.use(agendamentos, agendamentosRoutes) ao index.js

--- backend/index.js
+++ backend/index.js
@@ -10,6 +10,8 @@ const usuariosRoutes = require("./routes/usuarios");
 const ambienteRoutes = require("./routes/ambiente");
 // Importa as rotas relacionadas a autenticação de login
 const authRoutes = require("./routes/auth");
+// Importa as rotas relacionadas aos agendamentos
+const agendamentosRoutes = require("./routes/agendamentos")
 // Carrega as variáveis de ambiente do arquivo .env
 dotenv.config();
 // Conecta ao banco de dados MongoDB
@@ -26,6 +28,8 @@ app.use("/usuarios", usuariosRoutes);
 app.use("/", authRoutes);
 // Define que todas as rotas que começarem com /ambiente serão tratadas pelo arquivo de rotas de usuário
 app.use("/ambientes", ambienteRoutes);
+// POST /agendamentos
+app.use("agendamentos", agendamentosRoutes);
 // Rota principal (GET /) que apenas retorna uma mensagem simples
 app.get("/", (req, res) => {
   res.send("Servidor Express funcionando com MongoDB!");
